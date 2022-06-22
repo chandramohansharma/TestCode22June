236e29756b91cf509d505f365061c7ecc0fe752e
unknown <cmsharma2006@gmail.com>
2022-06-22 13:22:17 +0530

    demo code

---
+++ Invoicing System/ConsoleApp1/Customers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Customers : ICustomers
+    {
+        public List<Projects> Projects()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
